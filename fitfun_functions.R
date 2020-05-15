89b97dcbfd8891e6c31de5540d4e5bc1ba05c7e1
Dan Bramich <dan.bramich@hotmail.co.uk>
2020-05-15 15:49:56 +0000

    Completed code in fitfun_functions.R for finding the number of peaks and the maximum peak and its position in a run of numbers

--- fitfun_functions.R
+++ fitfun_functions.R
@@ -12,15 +12,6 @@ get_curve_properties_for_mu = function(reconstructed_model_fit, fd_type) {
 # Set the default output parameter values
 curve_properties = list(q_0 = -1.0, v_ff = -1.0, dvdk_0 = -1.0, k_crit = -1.0, k_vmax = -1.0, q_cap = -1.0, v_max = -1.0, n_peaks = -1, k_jam = -1.0, v_bw = -1.0)
 
-
-#######
-#reconstructed_model_fit$mu[1:971] = 0.0
-#reconstructed_model_fit$mu[10:21] = 1.0
-#reconstructed_model_fit$mu[30:43] = 1.0
-#fd_type = 'Speed.Density'
-#######
-
-
 # Compute some useful quantities
 ngrid = nrow(reconstructed_model_fit)
 grid_density_step = reconstructed_model_fit$V2[2]
@@ -65,20 +56,99 @@ if (index_mu_first_pos == -1) {
   return(curve_properties)
 }
 
+# Create a temporary vector that starts with a single zero value, followed by the first run of positive numbers in the "mu" curve, and ends with
+# another single zero value
+npos_p2 = index_mu_last_pos - index_mu_first_pos + 3
+tmpvec = double(length = npos_p2)
+tmpvec[2:(npos_p2 - 1)] = reconstructed_model_fit$mu[index_mu_first_pos:index_mu_last_pos]
+
+# Find the number of peaks in the first run of positive numbers in the "mu" curve while also finding the highest peak and its corresponding density.
+# In the case that two or more peaks are all equally the highest peak, then the peak with the smallest density is selected as the highest peak.
+curr_i = 2
+n_peaks = 0
+max_peak_value = -1.0
+max_peak_density = -1.0
+while (curr_i < npos_p2) {
+
+  # Extract the previous, current, and next values from the temporary vector
+  prev_tmpvec = tmpvec[curr_i - 1]
+  curr_tmpvec = tmpvec[curr_i]
+  next_tmpvec = tmpvec[curr_i + 1]
+
+  # If the previous value in the temporary vector is greater than the current value, then there is no peak at the current value
+  if (prev_tmpvec > curr_tmpvec) {
+
+    # If the next value in the temporary vector is not equal to the current value, then move on to the next value
+    if (next_tmpvec != curr_tmpvec) {
+      curr_i = curr_i + 1
+      next
+    }
+
+    # If the next value in the temporary vector is equal to the current value, then move on to the next value that is not equal to the current
+    # value
+    while (tmpvec[curr_i] == curr_tmpvec) { curr_i = curr_i + 1 }
+    next
+
+  # If the previous value in the temporary vector is less than the current value, then it is possible that there is a peak at, or near, the current
+  # value (N.B: Due to the way that this algorithm is designed, it is impossible that the previous value in the temporary vector is equal to the
+  # current value)
+  } else {
+
+    # If the next value in the temporary vector is greater than the current value, then there is no peak at the current value. Move on to the next
+    # value.
+    if (next_tmpvec > curr_tmpvec) {
+      curr_i = curr_i + 1
+      next
+    }
+
+    # If the next value in the temporary vector is less than the current value, then there is a peak at the current value. Count the peak and update
+    # the highest peak if necessary. Move on to the next value.
+    if (next_tmpvec < curr_tmpvec) {
+      n_peaks = n_peaks + 1
+      if (curr_tmpvec > max_peak_value) {
+        max_peak_value = curr_tmpvec
+        max_peak_density = reconstructed_model_fit$V2[curr_i + index_mu_first_pos - 2]
+      }
+      curr_i = curr_i + 1
+      next
+    }
+
+    # If the next value in the temporary vector is equal to the current value, then find the next value that is not equal to the current value
+    start_i = curr_i
+    while (tmpvec[curr_i] == curr_tmpvec) { curr_i = curr_i + 1 }
+
+    # If the next value that is not equal to the current value is greater than the current value, then there is no peak near to the current value.
+    # Move on to the next value that is not equal to the current value.
+    if (tmpvec[curr_i] > curr_tmpvec) { next }
+
+    # If the next value that is not equal to the current value is less than the current value, then there is a peak near to the current value.
+    # Count the peak and update the highest peak if necessary. Move on to the next value that is not equal to the current value.
+    n_peaks = n_peaks + 1
+    if (curr_tmpvec > max_peak_value) {
+      max_peak_value = curr_tmpvec
+      max_peak_density = 0.5*(reconstructed_model_fit$V2[start_i + index_mu_first_pos - 2] + reconstructed_model_fit$V2[curr_i + index_mu_first_pos - 3])
+    }
+  }
+}
+
 
 #### ABOVE FULLY READ AND TESTED
 
 
-zlen = index_mu_last_pos - index_mu_first_pos + 1 + 2
-zvec = double(length = zlen)
-zvec[2:(zlen - 1)] = reconstructed_model_fit$mu[index_mu_first_pos:index_mu_last_pos]
+cat('\n')
+cat(index_mu_first_pos, index_mu_last_pos, '\n')
 
-cat(zvec, '\n')
-
+cat('\n')
+cat(reconstructed_model_fit$V2[index_mu_first_pos:index_mu_last_pos], '\n')
+cat('\n')
+cat(tmpvec, '\n')
 
 
 cat('\n')
-cat(index_mu_first_pos, index_mu_last_pos, '\n')
+cat(n_peaks, '\n')
+cat(max_peak_value, '\n')
+cat(max_peak_density, '\n')
+
 q(save = 'no', status = 1)
 
 
