9762a70b03ede0cb4701710a16207ba26dda3226
Dan Bramich <dan.bramich@hotmail.co.uk>
2021-08-21 19:48:35 +0000

    Replaced format with sprintf because of weird behaviour with format

--- fit_speed_density_with_BD1995_GaussSigCon.R
+++ fit_speed_density_with_BD1995_GaussSigCon.R
@@ -321,10 +321,10 @@ if (!is.na(v_bw)) { cat('  Back-propagating wave speed at jam density:          
 if (!is.na(dvdk_kjam)) { cat('  Gradient of the speed (w.r.t. density) at jam density: ', sprintf('%.8g', dvdk_kjam), '\n') }
 cat('\n')
 cat('Fitted model parameters (see the accompanying papers by Bramich, Menendez & Ambuhl for details):\n')
-cat('  v_ff:     ', model_obj$mu.coefficients[1], '\n')
-cat('  c_1:      ', par1, '\n')
-cat('  c_2:      ', par2, '\n')
-cat('  sigma_con:', exp(model_obj$sigma.coefficients[1]), '\n')
+cat('  v_ff:     ', sprintf('%.8g', model_obj$mu.coefficients[1]), '\n')
+cat('  c_1:      ', sprintf('%.8g', par1), '\n')
+cat('  c_2:      ', sprintf('%.8g', par2), '\n')
+cat('  sigma_con:', sprintf('%.8g', exp(model_obj$sigma.coefficients[1])), '\n')
 
 # Write out the fit summary file "Fit.Summary.<fd_type>.<functional_form_model>.<noise_model>.txt"
 cat('\n')
@@ -341,10 +341,10 @@ tryCatch(
         '# FITTED MODEL PARAMETERS (SEE THE ACCOMPANYING PAPERS BY BRAMICH, MENENDEZ & AMBUHL FOR DETAILS)\n',
         '# N.B: FITTED COEFFICIENTS FOR ANY NON-PARAMETRIC SMOOTHING FUNCTIONS IN THE MODEL ARE NOT REPORTED HERE\n',
         '######################################################################################################################\n',
-        model_obj$mu.coefficients[1], '           # v_ff\n',
-        par1, '           # c_1\n',
-        par2, '           # c_2\n',
-        exp(model_obj$sigma.coefficients[1]), '           # sigma_con\n',
+        sprintf('%.8g', model_obj$mu.coefficients[1]), '           # v_ff\n',
+        sprintf('%.8g', par1), '           # c_1\n',
+        sprintf('%.8g', par2), '           # c_2\n',
+        sprintf('%.8g', exp(model_obj$sigma.coefficients[1])), '           # sigma_con\n',
         file = output_files[1], sep = '', append = TRUE) },
   error = function(cond) { cat('ERROR - Failed to write out the fit summary file...\n')
                            remove_file_list(output_files)
--- fit_speed_density_with_ED1961kjf_GaussSigCon.R
+++ fit_speed_density_with_ED1961kjf_GaussSigCon.R
@@ -424,11 +424,11 @@ if (!is.na(v_bw)) { cat('  Back-propagating wave speed at jam density:          
 if (!is.na(dvdk_kjam)) { cat('  Gradient of the speed (w.r.t. density) at jam density: ', sprintf('%.8g', dvdk_kjam), '\n') }
 cat('\n')
 cat('Fitted model parameters (see the accompanying papers by Bramich, Menendez & Ambuhl for details):\n')
-cat('  v_ff:     ', model_obj$mu.coefficients[1], '\n')
-cat('  1/k_crit: ', -par1, '\n')
-cat('  k_b:      ', par2, '\n')
-cat('  v_bw:     ', model_obj$mu.coefficients[2], '\n')
-cat('  sigma_con:', exp(model_obj$sigma.coefficients[1]), '\n')
+cat('  v_ff:     ', sprintf('%.8g', model_obj$mu.coefficients[1]), '\n')
+cat('  1/k_crit: ', sprintf('%.8g', -par1), '\n')
+cat('  k_b:      ', sprintf('%.8g', par2), '\n')
+cat('  v_bw:     ', sprintf('%.8g', model_obj$mu.coefficients[2]), '\n')
+cat('  sigma_con:', sprintf('%.8g', exp(model_obj$sigma.coefficients[1])), '\n')
 
 # Write out the fit summary file "Fit.Summary.<fd_type>.<functional_form_model>.<noise_model>.txt"
 cat('\n')
@@ -445,11 +445,11 @@ tryCatch(
         '# FITTED MODEL PARAMETERS (SEE THE ACCOMPANYING PAPERS BY BRAMICH, MENENDEZ & AMBUHL FOR DETAILS)\n',
         '# N.B: FITTED COEFFICIENTS FOR ANY NON-PARAMETRIC SMOOTHING FUNCTIONS IN THE MODEL ARE NOT REPORTED HERE\n',
         '######################################################################################################################\n',
-        model_obj$mu.coefficients[1], '           # v_ff\n',
-        -par1, '           # 1/k_crit\n',
-        par2, '           # k_b\n',
-        model_obj$mu.coefficients[2], '           # v_bw\n',
-        exp(model_obj$sigma.coefficients[1]), '           # sigma_con\n',
+        sprintf('%.8g', model_obj$mu.coefficients[1]), '           # v_ff\n',
+        sprintf('%.8g', -par1), '           # 1/k_crit\n',
+        sprintf('%.8g', par2), '           # k_b\n',
+        sprintf('%.8g', model_obj$mu.coefficients[2]), '           # v_bw\n',
+        sprintf('%.8g', exp(model_obj$sigma.coefficients[1])), '           # sigma_con\n',
         file = output_files[1], sep = '', append = TRUE) },
   error = function(cond) { cat('ERROR - Failed to write out the fit summary file...\n')
                            remove_file_list(output_files)
